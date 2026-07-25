06278ab3b0308a1d23f32081c59ee0f1d5049920
Carlos <carlos.2cmf@gmail.com>
2026-07-25 10:34:08 -0300

    Trnasformado em classe

--- Verificador_CPF.py
+++ Verificador_CPF.py
@@ -1,60 +1,78 @@
-def confere(cpf:list) -> bool:
-    '''Recebe uma lista contendo os números do CPF e retorna um booleano indicando se o CPF é válido'''
-    cpf_c = cpf[0:9]
-    #levantando o primeiro dígito verificador
-    somador = 0
-    contador = 10
-    for n in cpf_c:
-        somador += n*contador
-        contador -= 1
-    resto = somador % 11
-    if resto == 0 or resto == 1:
-        cpf_c.append(0)
-    else:
-        cpf_c.append(11 - resto)
+class CPF:
+    '''Classe que representa um CPF e contém métodos para validar e formatar o CPF, deve receber o CPF como uma string de 11 dígitos sem pontos e sem traço'''
     
-    #levantando o segundo dígito verificador
-    somador = 0
-    contador = 11
-    for n in cpf_c:
-        somador += n*contador
-        contador -= 1
-    resto = somador % 11
-    if resto == 0 or resto == 1:
-        cpf_c.append(0)
-    else:
-        cpf_c.append(11 - resto)
-    #comparando o CPF original com o montado dentro da função
-    if cpf == cpf_c:
-        return True
-    else:
-        return False
+    def __init__(self, cpf:str):
+        self.cpf = cpf
 
-def mostra_cpf(cpf:list) -> str:
-    '''Recebe uma lista de números do CPF digitado e retorna uma string com o CPF formatado'''
-    concat = ''
-    contador = 1
-    for x in cpf:
-        if contador % 3 == 0 and contador != 9:
-            concat+= str(x)
-            concat += '.'
-            contador += 1
+    def __str__(self) -> str:
+        return f'{self.formata_cpf()}'
+
+    def tranforma_lista(self) -> list:
+        '''Transforma o CPF em uma lista de números inteiros'''
+        return [int(x) for x in self.cpf]
+        
+
+    def confere(self) -> bool:
+        '''Confere se o CPF digitado é válido ou não'''
+        if len(self.cpf) != 11:
+            return False
+        if self.cpf == self.cpf[0]*11:
+            return False
+        cpf_c = self.tranforma_lista()[:9]
+        somador = 0
+        contador = 10
+        for n in cpf_c:
+            somador += n*contador
+            contador -= 1
+        resto = somador % 11
+        if resto == 0 or resto == 1:
+            cpf_c.append(0)
         else:
-            concat += str(x)
-            contador += 1
-        if contador == 10:
-            concat += '-'
-    return concat
+            cpf_c.append(11 - resto)
+        
+        somador = 0
+        contador = 11
+        for n in cpf_c:
+            somador += n*contador
+            contador -= 1
+        resto = somador % 11
+        if resto == 0 or resto == 1:
+            cpf_c.append(0)
+        else:
+            cpf_c.append(11 - resto)
+        #comparando o CPF original com o montado dentro da função
+        if self.tranforma_lista() == cpf_c:
+            return True
+        else:
+            return False
 
+    def formata_cpf(self) -> str:
+        '''Recebe uma lista de números do CPF digitado e retorna uma string com o CPF formatado'''
+        concat = ''
+        contador = 1
+        for x in self.cpf:
+            if contador % 3 == 0 and contador != 9:
+                concat+= str(x)
+                concat += '.'
+                contador += 1
+            else:
+                concat += str(x)
+                contador += 1
+            if contador == 10:
+                concat += '-'
+        return concat
 
-def main():
+"Teste de verificação de CPF"
+if __name__ == '__main__':
     while True:
+        print("Você está em loop infinito de verificação de CPF, para sair pressione Ctrl + C")
         try:
             #Solicitando o CPF ao usuário
             cpf = input('Digite o CPF (sem "." e e sem "-") --> ')
-            #transformando a string em uma lista de inteiros
-            cpf = [int(x) for x in cpf]
-            #checando se o CPF tem 11 dígitos
+            #verificando se o CPF digitado é um número e se tem 11 dígitos
+            if not cpf.isdigit():
+                print('\nVocê não digitou um número válido\n')
+                continue
             if len(cpf) > 11:
                 print('\nVocê colocou números demais, revise\n')
                 continue
@@ -62,21 +80,13 @@ def main():
                 print('\nEstá faltando números, confira novamente\n')
                 continue
             else:
-                break
-        except ValueError:
-            print(f'\n\nVocê não digitou um número válido\n')
+                cpf = CPF(cpf)
+                if cpf.confere():
+                    print(f'\n{cpf.formata_cpf()} --> VÁLIDO\n')    
+                else:
+                    print(f'\n{cpf.formata_cpf()} --> INVÁLIDO\n')       
         except KeyboardInterrupt:
-            print("\n\nUsuário desconectou pelo teclado\n")
+            print("\n\nAté mais! \n")
             break
-
-    #informando se o CPF é válido ou não
-    try:
-        if confere(cpf):
-                print(f'O CPF --> {mostra_cpf(cpf)} é VÁLIDO.')
-        else:
-            print(f'O CPF --> {mostra_cpf(cpf)} NÃO é válido')
-    except:
-        pass
-
-if __name__ == '__main__':
-    main()
+        except:
+            break
